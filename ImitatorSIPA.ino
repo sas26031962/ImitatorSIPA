66e1279b65f326600d035e47897b67e0dfbfba25
sas26031962 <sasx26031962@gmail.com>
2019-10-25 14:52:40 +0300

    initial commit

---
+++ ImitatorSIPA.ino
@@ -0,0 +1,271 @@
+/*
+ Программа для имитации СИПА
+ Собственный адрес платы: 192.168.1.4 (задаётся массивом ip)
+ Адрес управляющего узла: 192.168.1.1 (задаётся массивом ip0)
+
+ */
+ 
+//=====================================================================
+// Подключаемые модули
+//=====================================================================
+#include <SPI.h>         
+#include <Ethernet.h>
+#include <EthernetUdp.h>        
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include "cCommunication.h"
+#include "Global.h"
+#include "config.h"
+#include "sDisplay.h"
+
+//=====================================================================
+// Аппаратная конфигурация
+//=====================================================================
+// Область для управления OLED эканом
+#define OLED_MOSI   22
+#define OLED_CLK    24
+#define OLED_CS     26
+#define OLED_DC     28
+#define OLED_RESET  30
+
+#define KEY_1 7   // UP
+#define KEY_2 6   // DOWN
+#define KEY_3 5   // SELECT
+#define KEY_4 8   // RESTART
+
+//=====================================================================
+// Константы
+//====================================================================
+#define REPEATE_TIME 500 // время повтора посылок
+
+#define BUFFER_SIZE 26  // размер буфера для передачи
+#define PACKET_LENGTH 32 // размер буфера для приёма
+
+//=====================================================================
+// Глобальные переменные
+//=====================================================================
+//
+// IP адреса передатчика и приёмника
+//
+char ip0[4] = {192, 168, 1, 4}; 
+char ip1[4] = {192, 168, 1, 1}; 
+
+IPAddress local_ip(ip0[0], ip0[1], ip0[2], ip0[3]);
+IPAddress remote_ip(ip1[0], ip1[1], ip1[2], ip1[3]);
+
+//
+// Локальный порт, который слушает программа
+// 
+unsigned int localPort = 25411;      
+unsigned int destinationPort = 25104;      
+
+//
+// Буфера для приёма и передачи данных
+//
+char packetBuffer[BUFFER_SIZE];  // буфер для хранения входящего пакета,
+
+char AnswerBuffer[BUFFER_SIZE];
+//
+// Экземпляр класса для реализации передачи и приёма информации по протоколу UDP
+//
+EthernetUDP Udp;
+
+// 
+// Параметры принятого пакета
+//
+int packetSize = 0;                             // размер принятого пакета
+
+//
+// Число принятых пакетов
+//
+int SinkPacketCounter = 0;
+int SendPacketCounter = 0;
+
+
+// Переменные для управления потоком по времени
+long CurrentTime;
+long PreviousTime;
+
+cCommunication Com;//Коммуникационная составляющая
+
+//
+//Область сетевых настроек
+//
+byte Global::mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE};
+
+
+Adafruit_SSD1306 sDisplay::display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+byte CheckSumView = 0;
+
+//=====================================================================
+// Начальная установка
+//=====================================================================
+void setup() 
+{
+  //
+  // Инициализация микросхемы Ethernet
+  //
+  Ethernet.begin(Global::mac, local_ip);     //Запуск Ethernet
+  Udp.begin(localPort);                      // Запуск UDP на приём
+
+  // Инициализация кнопок управления
+  pinMode(KEY_1, INPUT_PULLUP);  
+  pinMode(KEY_2, INPUT_PULLUP);  
+  pinMode(KEY_3, INPUT_PULLUP);  
+  pinMode(KEY_4, INPUT_PULLUP);  
+
+  //Запуск программного таймера
+  CurrentTime = millis();
+  PreviousTime = CurrentTime;
+
+  // Инициализация
+  sDisplay::display.begin(SSD1306_SWITCHCAPVCC);
+  
+  //
+  // Отображение картинки по умолчанию
+  // 
+  sDisplay::display.display();
+  delay(200);
+
+  viewData();
+  
+}//End of void setup()
+
+//=====================================================================
+// Главный цикл
+//=====================================================================
+void loop()
+{
+  CurrentTime = millis();
+  if(CurrentTime - PreviousTime > REPEATE_TIME)
+  { 
+    PreviousTime = CurrentTime;
+    if(true)
+    { 
+      // Подготовка пакета данных
+      prepareDataToSend();
+      //Передача пакета
+      Udp.beginPacket(remote_ip, destinationPort);
+      Udp.write( AnswerBuffer,PACKET_LENGTH);
+      Udp.endPacket();
+    
+      //Подсчёт переданных пакетов
+      SendPacketCounter++;
+    }
+
+    viewData();
+    
+  }//End of if(CurrentTime - PreviousTime > REPEATE_TIME)
+
+  // Приём пакета из буфера
+  packetSize = Udp.parsePacket();
+  
+  // 
+  // Разбор принятого пакета
+  //
+  if (packetSize > 0) 
+  { 
+
+    SinkPacketCounter++;// Подсчёт пакетов
+    Udp.read(packetBuffer, PACKET_LENGTH);
+
+    // Визуализация принятых данных
+    
+    
+    // Очистка буфера пакетов
+    for(int i = 0; i < PACKET_LENGTH; i++) packetBuffer[i] = ' ';
+  }
+
+}//End of void loop()
+
+//=====================================================================
+// Подпрограммы
+//=====================================================================
+//
+// Вывод на дисплей
+//
+void viewData()
+{
+  //Очистка дисплея
+  sDisplay::display.clearDisplay();
+  //Установка параметров дисплея
+  sDisplay::display.setTextSize(1);
+  sDisplay::display.setTextColor(WHITE);
+  sDisplay::display.setCursor(0,0);
+  //Формирование текста
+  sDisplay::display.println("ImitatorSIPA v1.0.0");
+  sDisplay::display.print("LocalIP:");//
+  sDisplay::display.println("192.168.1.4");// 
+  sDisplay::display.print("Send:");// 
+  sDisplay::display.println(SendPacketCounter);// 
+  sDisplay::display.print("CheckSum:");// 
+  sDisplay::display.println(CheckSumView);// 
+  //Вывод на дисплей
+  sDisplay::display.display();
+}//End of void viewData()
+
+/// <summary>
+/// Подготовка данных для передачи
+/// </summary>
+void prepareDataToSend()
+{
+  //
+  // Формирование ответного пакета
+  //        
+  int CheckSum = 0;
+  //-----------------------------------
+  //Формирование ответного пакета
+  AnswerBuffer[0] = 2;//Номер версии
+  AnswerBuffer[1] = 0;//Команда
+  AnswerBuffer[2] = 0;//Код ошибки принятого сообщения
+        
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //Период усреднения
+  AnswerBuffer[3 + 0] = 1;//!!!this.SIPA.btAverage;
+  //Период передачи
+  AnswerBuffer[3 + 1] = 2;//!!!this.SIPA.btRequest;
+  //Температура
+  AnswerBuffer[3 + 2] = 3;//this.SIPA.Temperature.Byte0; 
+  AnswerBuffer[3 + 3] = 17;//this.SIPA.Temperature.Byte1;
+  //Влажность
+  AnswerBuffer[3 + 4] = 5;//this.SIPA.Humidity.Byte0; 
+  AnswerBuffer[3 + 5] = 53;//this.SIPA.Humidity.Byte1;
+  //Атмосферное давление
+  AnswerBuffer[3 + 6] = 0;//PRESSURE0;
+  AnswerBuffer[3 + 7] = 3;//PRESSURE1;
+  AnswerBuffer[3 + 8] = 200;//PRESSURE2;//this.SIPA.Pressure.Byte2;
+  AnswerBuffer[3 + 9] = 3;//PRESSURE3;//this.SIPA.Pressure.Byte3;
+  //Средняя скорость ветра
+  AnswerBuffer[3 + 10] = 0;//!!!this.SIPA.SpeedAverage.Byte0;
+  AnswerBuffer[3 + 11] = 0;//!!!this.SIPA.SpeedAverage.Byte1;
+  //Минимальная скорость ветра
+  AnswerBuffer[3 + 12] = 0;//!!!this.SIPA.SpeedMin.Byte0;
+  AnswerBuffer[3 + 13] = 0;//!!!this.SIPA.SpeedMin.Byte1;
+  //Максимальная скорость ветра
+  AnswerBuffer[3 + 14] = 0;//!!!this.SIPA.SpeedMax.Byte0;
+  AnswerBuffer[3 + 15] = 0;//!!!this.SIPA.SpeedMax.Byte1;
+  //Среднее направление ветра
+  AnswerBuffer[3 + 16] = 0;//!!!this.SIPA.DirectionAverage.Byte0;
+  AnswerBuffer[3 + 17] = 0;//!!!this.SIPA.DirectionAverage.Byte1;
+  //Минимальное направление ветра
+  AnswerBuffer[3 + 18] = 0;//!!!this.SIPA.DirectionMin.Byte0;
+  AnswerBuffer[3 + 19] = 0;//!!!this.SIPA.DirectionMin.Byte1;
+  //Максимальное направление ветра
+  AnswerBuffer[3 + 20] = 0;//!!!this.SIPA.DirectionMax.Byte0;
+  AnswerBuffer[3 + 21] = 0;//!!!this.SIPA.DirectionMax.Byte1;
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  CheckSum = 0;
+  for(int i = 0; i < BUFFER_SIZE - 1; i++)
+  {
+    CheckSum += AnswerBuffer[i];
+  }
+  AnswerBuffer[25] = (byte)(CheckSum & 255);
+
+  CheckSumView = (byte)(CheckSum & 255);
+      
+}//End of void prepareDataToSend()
+    
+
---
+++ cCommunication.h
@@ -0,0 +1,278 @@
+/*
+ Класс управления коммуникацией по последовательному порту
+ */
+ //========================================================================
+ //---------------------------- ПОДКЛЮЧАЕМЫЕ ФАЙЛЫ ------------------------
+ //========================================================================
+#include <SD.h>
+
+ 
+ //========================================================================
+ //-------------------------------- КОНСТАНТЫ -----------------------------
+ //========================================================================
+ #define SINK_BUFFER_LENGTH 20
+ 
+ #define SERIAL_SPEED 115200
+ #define MARKER '{'
+ #define SEARCH_MARKER_PHASE 0
+ #define GET_FRAME_BODY_PHASE 1
+
+ #define BUFFER_OVERFLOW_ERROR -1
+
+ #define COMMAND_DISPLACEMENT 1
+ #define COLON_DISPLACEMENT COMMAND_DISPLACEMENT+1
+ #define PARAMETER_DISPLACEMENT COLON_DISPLACEMENT+1
+
+ //========================================================================
+ //--------------------------- ОБЪЯВЛЕНИЕ КЛАССОВ -------------------------
+ //========================================================================
+
+class cCommunication{
+ 
+ //------------------------------------------------------------------------
+ // Атрибуты
+ //------------------------------------------------------------------------
+ private:
+ public:
+  int Phase = 0;//Фаза процесса коммуникации
+                // 0 - поиск маркера
+                // 1 - приём тела кадра
+  char SinkBuffer[20] = {
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',                   
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '                   
+    }; //Массив SINK_BUFFER_LENGTH байт
+  int SinkCounter = 0;
+
+  char x; //Текущий принятый байт
+
+  String Parameter; //Строка, содержащая значение параметра
+  
+  bool IsRemoteControl;   //Флаг управления от удалённого терминала
+  
+ public:
+
+ 
+ //------------------------------------------------------------------------
+ // Методы
+ //------------------------------------------------------------------------
+
+ //Инициализация коммуникационного порта
+ void install();
+ 
+ //Реализация функциональности
+ void operate();    
+
+ //Реализация фазы поиска маркера
+ void searchMarker();
+
+ //Приём одного байта в буфер, возвращает принятый байт
+ char sinkByte();
+
+ //Эхо принятого пакета
+ void echoFrame();
+
+ //Чтение параметра
+ void getParameter();
+
+ //------------------------------------------------------------------------
+ // Конструкторы
+ //------------------------------------------------------------------------
+ cCommunication();
+ 
+};//End of class cCommunication
+
+ //========================================================================
+ //--------------------------- РЕАЛИЗАЦИЯ МЕТОДОВ -------------------------
+ //========================================================================
+
+//
+// Конструктор
+//
+cCommunication::cCommunication() 
+{
+  
+  this->Phase = 0;
+  this->SinkCounter = 0;
+  this->IsRemoteControl = false;
+  
+}//End of ctor 
+
+//
+// Инициализация коммуникационного порта
+//
+void cCommunication::install()
+{
+  Serial.begin(SERIAL_SPEED);
+  while (!Serial) {;}
+}//End of void cCommunication::install()
+
+//
+//Реализация фазы поиска маркера
+// 
+//
+void cCommunication::searchMarker()
+{
+  if(this->x == MARKER)
+  {
+    this->SinkCounter = 0;
+    this->SinkBuffer[this->SinkCounter] = this->x;
+    this->SinkCounter++;
+    this->Phase = GET_FRAME_BODY_PHASE;
+        
+    //Serial.println("Marker detected, goto sink frame body");
+  }      
+}//End of void cCommunication::searchMarker()
+
+//
+// Приём одного байта в буфер, возвращает принятый байт
+//
+char cCommunication::sinkByte()
+{
+  this->SinkBuffer[this->SinkCounter] = this->x;
+  this->SinkCounter++;// Проверка границы массива
+  //Serial.print(this->x);//Эхо для проверки
+  //Проверка границы массива
+  if(this->SinkCounter == SINK_BUFFER_LENGTH)
+  {
+    this->Phase = 0;
+    //Serial.println(); //Конец эха
+    Serial.println("Right baund array error, goto SEARCH_MARKER phase");
+    return this->x;
+  }
+  else
+  {
+    return BUFFER_OVERFLOW_ERROR;
+  }
+}//End of void cCommumnication::sinkByte()
+
+//
+//Эхо принятого пакета
+//
+void cCommunication::echoFrame()
+{
+  for(int i = 0; i < this->SinkCounter; i++)Serial.print(this->SinkBuffer[i]);
+  Serial.println();
+}
+ 
+//Чтение параметра
+void  cCommunication::getParameter()
+{
+  if(this->SinkBuffer[2]!= ':')
+  {
+    Serial.println("Colon missing error");
+    this->Phase = SEARCH_MARKER_PHASE;
+  }
+  else
+  {
+    this->Parameter = "";
+    int Pointer = 3;//Смещение для параметра
+    while((this->SinkBuffer[Pointer] != '}')&&(this->SinkBuffer[Pointer] != ',')&&(Pointer < SINK_BUFFER_LENGTH - 3))
+    {
+      this->Parameter += this->SinkBuffer[Pointer];
+      Pointer++;
+    }
+  }    
+  // Отладка
+  Serial.print("Parameter: ");
+  Serial.println(this->Parameter);
+  
+}//End of void  cCommunication::getParameter()
+
+//
+// Реализация функциональности
+//
+void cCommunication::operate()
+{
+  if(Serial.available())
+  {
+    this->x = Serial.read();
+    switch(this->Phase)
+    {
+      case 0: this->searchMarker(); break;
+
+      case 1:
+        this->sinkByte();
+        if(this->x == '}')
+        {
+          //Serial.println("Sink frame complete, goto frame analisys");
+          //Com.echoFrame();          
+          
+          //
+          //... здесь анализ пакета
+          //
+          switch(this->SinkBuffer[1])
+          {
+            case 'D':// Удаление файла
+            Serial.println("Delete file command");
+            this->getParameter();
+            break;
+            //-------------------------------------
+            case 'E':// Проверка существования файла
+            Serial.println("Check exist file command");
+            this->getParameter();
+            break;
+            //-------------------------------------
+            case 'R':// Чтение файла
+            Serial.println("Read file command");
+            this->getParameter();
+            break;
+            //-------------------------------------
+            case 'L':// Чтение списка файлов
+            Serial.println("Read file list command");
+            this->getParameter();
+            break;
+            
+            //-------------------------------------
+            case 'C':// Управление удалённым контролем
+            Serial.println("Remote control command");
+            this->getParameter();
+            // Выполнение команды
+            break;
+            
+            //-------------------------------------
+            case 'T':// Чтение времени
+            Serial.println("Get time command");
+            this->getParameter();
+            // Выполнение команды
+            break;
+            
+            //-------------------------------------
+            case 'F':// Чтение параметров файла
+            Serial.println("Read file parameters command");
+            this->getParameter();
+            // Выполнение команды
+            break;
+            
+            //-------------------------------------
+            case 'X':// Управление контроллером
+            Serial.println("Direct control command");
+            this->getParameter();
+            // Выполнение команды
+            break;
+            
+            //-------------------------------------
+            
+            default:
+            Serial.print("Unknown frame command: ");
+            Serial.println(this->SinkBuffer[1]);
+            break;  
+          }//End of switch(SinkBuffer[1])
+          
+          this->Phase = SEARCH_MARKER_PHASE;
+          //Serial.println("Frame analisys complete, goto SEARCH_MARKER phase");
+          Serial.print(">");
+        }
+      break;
+
+      default:
+        this->Phase = SEARCH_MARKER_PHASE;
+        Serial.println("Unknown command, goto SEARCH_MARKER phase");
+      break;  
+    }//End of switch(Com.Phase) 
+    
+  }//End of if(Serial.available())
+}//End of void cCommunication::operate()
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+
+
